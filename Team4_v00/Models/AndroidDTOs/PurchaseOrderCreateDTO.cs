84f201f4feba59ac3e413334e9a3ddd8affc48ec
DESKTOP-3QUAP8A\hanhn <hanhnguyen0301@gmail.com>
2020-08-23 21:57:07 +0800

    Summer update Email's content (PO)

---
+++ Team4_v00/Models/AndroidDTOs/PurchaseOrderCreateDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ben_Project.Models.AndroidDTOs
+{
+    public class PurchaseOrderCreateDTO
+    {
+        public string OrderDate { get; set; }
+        public string SupplierName { get; set; }
+    }
+}
